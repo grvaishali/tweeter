462f9f62f77e17a83b7e90e9b10747bcaad2d84e
Vaishali Gupta <grvaishali@gmail.com>
2020-09-09 22:31:56 +0000

    Fixed header

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".new-tweet textarea").on('keyup', function () {
+    let l = $('#tweet-text').val().length;
+    $('.counter').val(`${140 - l}`);
+
+    if (l > 140) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'inherit');
+    }
+  });
+});
